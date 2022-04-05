62dd3b4e4527ec2dc308d3e6b44b589ea2bf3ce9
Warren Wang <warren.wang2020@gmail.com>
2022-04-04 21:29:53 -0400

    removed personal banner from main file

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,18 +1,3 @@
-/* *****************************************
- * CSCI205 - Software Engineering and Design
- * Spring2022
- * Instructor: Brian King
- * Section: 1 - 10 am
- *
- * Name: Warren Wang
- * Date: 01/26/2022
- *
- * Lab / Assignment:
- *
- * Description:
- *
- * *****************************************/
-
 
 package main;
 
